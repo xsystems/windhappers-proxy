07ec9f7e3b78e165a77bf2754e4c1a89771479f4
Koen Boes <kboes@qualogy.com>
2020-05-25 00:12:06 +0200

    Update paths in start script

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 for DOMAIN in ${LETSENCRYPT_DOMAINS} ; do
-  while [ ! -f /etc/letsencrypt/live/${DOMAIN}/full.pem ]; do sleep 1; done
+  echo "Waiting for file to become available: /etc/letsencrypt/full/${DOMAIN}.pem"
+  while [ ! -f /etc/letsencrypt/full/${DOMAIN}.pem ]; do sleep 1; done
+  echo "File available: /etc/letsencrypt/full/${DOMAIN}.pem"
 done
 
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
